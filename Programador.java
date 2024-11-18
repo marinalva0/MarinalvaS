c463fc1cfd0d62fb925f4afaccf611421bab7fc9
Marinalva0 <164238004+marinalva0@users.noreply.github.com>
2024-11-18 09:22:36 -0300

    Add files via upload

---
+++ Programador.java
@@ -0,0 +1,17 @@
+public class Programador extends Fucionario {
+    private String linguagem;
+    private String informalinguagem;
+
+    public void setlinguagem(String linguagem) {
+        this.linguagem = linguagem;
+    }
+    public String getlinguagem() {
+        return linguagem;
+    }
+    public void setInformalinguagem(String linguagem) {
+        this.informalinguagem = informalinguagem;
+    }
+    public String getInformalinguagem() {
+        return informalinguagem;
+    }
+}
