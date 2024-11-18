ada7273d3c3dd8d865522d164b78ab1d130cb74f
Marinalva0 <164238004+marinalva0@users.noreply.github.com>
2024-11-18 09:25:06 -0300

    Add files via upload

---
+++ Cachorro.java
@@ -0,0 +1,14 @@
+public class Cachorro extends Animal{
+    private String late;
+
+
+    public void setlate(String late){
+        this.late = late;
+    }
+
+    public String getlate(){
+        return late;
+    }
+
+
+}
