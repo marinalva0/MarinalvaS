dc64987632aa39bf39544f7a481e0f0f7e71e2c6
Marinalva0 <164238004+marinalva0@users.noreply.github.com>
2024-11-18 09:21:48 -0300

    Add files via upload

---
+++ Gerente.java
@@ -0,0 +1,18 @@
+public class Gerente extends  Fucionario{
+
+private String projeto;
+private String informaprojeto;
+
+public void setProjeto(String projeto) {
+    this.projeto = projeto;
+}
+public String getProjeto(){
+    return projeto;
+}
+public void setInformaprojeto(String informaprojeto){
+    this.informaprojeto = informaprojeto;
+}
+public String getInformaprojeto(){
+    return informaprojeto;
+}
+}
