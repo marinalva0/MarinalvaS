459b83ddcf6889100af7304b6c4711460137b336
Marinalva0 <164238004+marinalva0@users.noreply.github.com>
2024-11-18 09:24:42 -0300

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,25 @@
+public class Animal {
+protected String nome;
+protected String raca;
+protected String caminha;
+
+
+public void setNome(String nome){
+    this.nome = nome;
+}
+public String getNome(){
+    return nome;
+}
+public void setRaça(String raca){
+    this.raca = raca;
+}
+public String getRaça(){
+    return raca;
+}
+public void setCaminha(String caminha){
+    this.caminha = caminha;
+}
+public String getCaminha(){
+    return caminha;
+}
+}
